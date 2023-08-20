b710c228fb2f83e6c09b13cf24ef3b13166f8e58
lateeeef <olalekanoluwa2@gmail.com>
2023-08-20 22:27:43 +0100

    least

--- index.php
+++ index.php
@@ -15,146 +15,14 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
 <body>
-    <?php
-        include 'includes/header.php';
-    ?>
     <main class="px-5 mt-2">
-    <a href="login.php">Login page</a>
+    <a href="login.php">Student</a>
     <a href="instructor/dashboard.php">Staff</a>
     <a href="admin/dashboard.php">Admin</a>
 
-        <div class="d-md-flex align-items-center mb-3  py-5 px-4 ">
-            <div class="col-md-7 pe-5 ">
-                <div class="display-4 fw-bold ">The Best Way to <br> Manage Your Finances</div>
-                <p class="fs-5">Get your finances, savings and budget under control all in one platform</p>
-                <a href="signup.php" class="btn btn-primary b-block">Create a free account</a>
-            </div>
-            <div class="col-md-5  d-md-flex justify-content-center py-3">
-                <img class="" src="images/fluxpay.webp" width="" height="" alt="">
-            </div>
-        </div>
 
-        <div class="d-md-flex justify-content-between my-3 px-4">
-            <div class=" col-md-4 pt-5 px-4 ">
-                <h3>Why choose Fluxpay?</h3>
-                <p class="pe-5">We are an impact driven financial technology platform that offers a comprehensive and
-                    convenient solution fro managing your finances</p>
-            </div>
-            <div class="row gap-2  d-md-flex justify-content-center">
-                <div class="col-md-5 p-3 ">
-                    <img src="images/convenience.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Convenience</h5>
-                    <p>We make it easy and convenientfor users t manage their finances on-the-go.</p>
-                </div>
-                <div class="col-md-5 p-3 ">
-                    <img src="images/security.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Security</h5>
-                    <p>We pritorize the secutity of our user's information and funds with advanced security measures.
-                    </p>
-                </div>
-                <div class="col-md-5 p-3 ">
-                    <img src="images/innovation.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Innovation</h5>
-                    <p>We leverage the latest technology to offer cutting edge financial services and solutions.</p>
-                </div>
-                <div class="col-md-5 p-3 ">
-                    <img src="images/savings.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Savings</h5>
-                    <p>We help users save money through features </p>
-                </div>
-            </div>
-        </div>
 
-        <div class="d-md-flex align-items-center my-3 py-5 px-4">
-            <div class="col-md-5 text-center rounded me-4 py-5">
-                <img src="images/fluxdashdsk.png" alt="" width="400" height="300">
-            </div>
-            <div class="p-5">
-                <h3>It only takes 2 minutes to get started</h3>
-                <p>Getting started on Fluxpay is incredibly easy. All you need to do is craete an account with a few
-                    simple steps and you are ready to go.Our user-friendly interface and straight-forward sign-up
-                    process make it simple to get started and start making transactions right away.</p>
-                <a href="signup.php" class="btn btn-primary">Get Started</a>
-            </div>
-        </div>
 
-        <div class="d-md-flex align-items-center justify-content-center my-5 ">
-            <div class="row gap-2 col-md-8 py-2 d-md-flex justify-content-center">
-                <div class="col-md-5 p-3 card">
-                    <img src="images/mobilebanking.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Mobile Banking</h5>
-                    <p>Ability to manage financs from your mobile devices and allows you to easily check your balance,
-                        make payments, and transfer money.</p>
-                </div>
-                <div class="col-md-5 p-3 card">
-                    <img src="images/realtime.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Real-time Transactions</h5>
-                    <p>Make real-time transactions through our website by ensuring your finances are always up-to-date
-                        and accessible.</p>
-                </div>
-                <div class="col-md-5 p-3 card">
-                    <img src="images/ai.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Artificial Intelligence</h5>
-                    <p>AI and machine learning algirithms to provide you with personalised investment recommendations,
-                        helping you to make informed decisions about your finances.</p>
-                </div>
-                <div class="col-md-5 p-3 card">
-                    <img src="images/budget.png" alt="" width="45" height="45">
-                    <h5 class="my-2">Budgeting and Savings</h5>
-                    <p>Tools for budgeting and saving such as arithmetic expence tracking and personalized saving tips,
-                        which make you manage your expenses more effectively. </p>
-                </div>
-            </div>
-            <div class="d-flex justify-content-center rounded-pill p-4 my-4">
-                <img src="images/fluxpaygirl.png" class="rounded-pill border border-dark p-3" alt="" width="300"
-                    height="300">
-            </div>
-        </div>
-
-        <div class="my-5 py-5 px-2">
-            <div class="text-center mb-5">
-                <h3>From the people who use Fluxpay</h3>
-                <p class="">Our mission in Fluxpay is to empower more people just like you to archive your personal
-                    financial goal.</p>
-            </div>
-            <div class="d-md-flex d-grid gap-3">
-                <div class="card p-3">
-                    <div class="h-75">
-                        <p class="mb-5">I've been using the Fluxpay website for several months now and it has completely
-                            changed the way I manage my finances. The mobile banking feature make it incredibly convenient
-                            to check my balance and make payment on-the-go.</p>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <img src="images/rachael.jpg" alt="" width="60" height="60" class="rounded-pill me-2">
-                        <div class="fw-bold">Rachael O.</div>
-                    </div>
-                </div>
-                <div class="card p-3">
-                    <div class="h-75">
-                        <p class="mb-5">What I love most about Fluxpay is the investment recommendations. The recommendations have helped me made much more infoemeddecisions about my investments and I've seen great results so far.</p>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <img src="images/john.jpg" alt="" width="60" height="60" class="rounded-pill me-2">
-                        <div class="fw-bold">John K.</div>
-                    </div>
-                </div>
-                <div class="card me p-3">
-                    <div class="h-75">
-                        <p class="mb-5">I'm so glad I found this platform. It has made managing my finances so much easier and more efficient and I would highly recommend it to anyone looking for a better way to manage thier money.</p>
-                    </div>
-                    <div class="d-flex align-items-center">
-                        <img src="images/cynthia.jpg" alt="" width="60" height="60" class="rounded-pill me-2">
-                        <div class="fw-bold">Cynthia G.</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="">
-            <img src="images/cryptoflux.jpg" class="w-100 rounded" alt="">
-        </div>
-
-    <?php include 'includes/footer.php' ?>
     </main>
     <?php
     
